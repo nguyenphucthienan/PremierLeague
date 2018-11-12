c1a77ef77f51227e592ef92801dcdd8f875d1c1b
Nguyen Phuc Thien An <npta97@gmail.com>
2018-11-12 14:38:02 +0700

    Update Players component

--- src/app/players/players-routing.module.ts
+++ src/app/players/players-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BriefClubsResolver } from '../core/resolvers/brief-clubs.resolver';
+import { BriefSeasonsResolver } from '../core/resolvers/brief-seasons.resolver';
 import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
 import { PlayersComponent } from './players.component';
 import { PlayerResolver } from './resolvers/player.resolver';
-import { PlayersResolver } from './resolvers/players.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: PlayersComponent,
     resolve: {
-      clubs: BriefClubsResolver,
-      players: PlayersResolver
+      seasons: BriefSeasonsResolver,
+      clubs: BriefClubsResolver
     }
   },
   {
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Club } from '../core/models/club.interface';
 import { FilterMode } from '../core/models/filter-mode.interface';
 import { Pagination } from '../core/models/pagination.interface';
 import { Player } from '../core/models/player.interface';
+import { Season } from '../core/models/season.interface';
 import { SortMode } from '../core/models/sort-mode.interface';
 import { PlayerService } from '../core/services/player.service';
-import { FilterOption } from '../shared/components/filter-bar/models/filter-option.interface';
 
 @Component({
   selector: 'app-players',
@@ -15,52 +16,39 @@ import { FilterOption } from '../shared/components/filter-bar/models/filter-opti
 })
 export class PlayersComponent implements OnInit {
 
+  seasons: Season[];
+  clubs: Club[];
   players: Player[];
-  pagination: Pagination;
-  filterOptions: FilterOption[] = [];
 
-  sortMode: SortMode = {
+  pagination: Pagination = {
+    pageNumber: 1,
+    pageSize: 20
+  };
+
+  private sortMode: SortMode = {
     sortBy: 'name',
     isSortAscending: true
   };
 
-  filterMode: FilterMode;
+  private filterMode: FilterMode = {};
 
   constructor(private route: ActivatedRoute,
     private playerService: PlayerService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.players = data['players'].items;
-      this.pagination = data['players'].pagination;
+      this.seasons = data['seasons'];
+      this.clubs = data['clubs'];
 
-      const clubOption: FilterOption = {
-        name: 'clubId',
-        text: 'Club',
-        placeholder: 'Select club',
-        values: data['clubs'].map(club => ({
-          text: club.name,
-          value: club.id
-        }))
-      };
-
-      const positionOption: FilterOption = {
-        name: 'position',
-        text: 'Position',
-        placeholder: 'Select position',
-        values: [
-          { text: 'Goalkeeper', value: 'goalkeeper' },
-          { text: 'Defender', value: 'defender' },
-          { text: 'Midfielder', value: 'midfielder' },
-          { text: 'Forward', value: 'forward' },
-        ]
-      };
-
-      this.filterOptions = [clubOption, positionOption];
+      this.getPlayers();
     });
   }
 
   getPlayers() {
+    if (!this.filterMode.seasonId && this.filterMode.clubId) {
+      return;
+    }
+
     this.playerService.getPlayers(this.pagination,
       this.sortMode, this.filterMode)
       .subscribe((response: any) => {
@@ -76,6 +64,16 @@ export class PlayersComponent implements OnInit {
 
   onFiltered(filterMode: FilterMode) {
     this.filterMode = filterMode;
+    this.getPlayers();
+  }
+
+  onSeasonFilterChanged(season: Season) {
+    this.filterMode.seasonId = season ? season.id : null;
+    this.getPlayers();
+  }
+
+  onClubFilterChanged(club: Club) {
+    this.filterMode.clubId = club ? club.id : null;
     this.getPlayers();
   }
 
